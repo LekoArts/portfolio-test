2b60579ff61aa80dce7cad033760d208899ffe8c
Matt Kane <matt@gatsbyjs.com>
2020-04-20 10:07:59 +0100

    Update packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
     `gatsby-plugin-sass`,
     `gatsby-transformer-remark`,
     {
-      resolve: `gatsby-source-datocms`,
+      resolve: `@gatsbyjs/gatsby-source-datocms`,
       options: {
         apiToken: process.env.DATO_API_TOKEN,
       },
